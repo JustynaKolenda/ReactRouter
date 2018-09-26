ec67d152936a5fc852d749b91d791b752ffee172
JustynaKolenda <j.trybalaa@gmail.com>
2018-09-27 00:18:41 +0200

    Wyświetlenie pojedynczego produktu
    -dodanie strony z pojedynczym produktem
    -konfiguracja rotingu z pojedynczym produktem
    -zmiana w widoku listy produktów : przejsćie do pojedynczego produktu z
    listy po kliknięciu na produkt
     Changes to be committed:
    	modified:   src/index.js
    	deleted:    src/list.js
    	new file:   src/listPage.js
    	new file:   src/productPage.js

--- src/index.js
+++ src/index.js
@@ -3,14 +3,16 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './index.css';
 import {App,App2} from './App';
-import {List} from './list';
+import {ListPage} from './listPage';
 import registerServiceWorker from './registerServiceWorker';
+import { ProductPage } from './productPage';
 
 ReactDOM.render((
     <BrowserRouter>
       <Switch>
+        <Route path = {"/list/product/:element"} component= {ProductPage} />
         <Route path = {"/cos/:page2"} component= {App2} />
-        <Route path = {"/list"} component= {List} />  
+        <Route path = {"/list"} component= {ListPage} />  
         <Route component={App} />    
       </Switch>
     </BrowserRouter>
---
+++ src/listPage.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import './App.css';
+import { NavLink } from 'react-router-dom';
+
+export class ListPage extends Component {
+    constructor() {
+        super();
+        /*
+        W this.state w constructor definiuje się początkowy stan komponentu.
+        Edycja state nie może być w renderze i componentDidUpdate.
+        Zmienia sie go za pomocą setState.
+        Cykl życia komponentu przy zmianie stata wygląda następująco:
+            - zmiana wartości w state komponentu
+            - urochomienie render
+            - uruchomienie componentDidUpdate
+        */
+        this.state = {
+                    produkts: []
+                }
+        }
+
+      /*
+      Render zawsze musi zwracać "pseudo htmla (jsx).""
+      Zawsze musi być zdefiniowany w komponencie.
+      */  
+    render() {
+       // console.log(this.props)
+        return(
+            /* 
+            Poprzez użycie funkcji map nie trzeba sprawdzać czy tablica jest pusta, bo jeżeli jest pusta to się nie wykona
+            funkcje map możemy użyć tylko przy tablicy inaczej poda błąd.
+            Wszystkie atrybuty w tagu htmlowym to propsy w jsx.
+            */
+            <ul>{this.state.produkts.map((el,i) => {
+                return <li  key={i} >
+                         <NavLink to={"/list/product/"+ el.id}>
+                            {el.name}
+                            <b>{el.price}</b>
+                            {(el.images && el.images.length > 0) &&<img src={el.images[0].src} alt=""/>}
+                        </NavLink>
+                        </li>
+            })} </ul>
+        );
+    }
+    /*
+    Jeżeli jej nie zdefinuije to jest ona zdefiniowana domyślnie w klasie komponent.
+    Uruchamia sie gdy komponent został już zamontowany, czyli potym jak construktor tworzy obiekt, render drukuje widok.
+    Można go porównać do documentRedy z JQuery.
+    */
+    componentDidMount() {
+        fetch("https://wp-api.kolendateam.pl/wp-json/wc/v2/products/?consumer_key=ck_e0df816b041c946561d4d0ebcdd9d8b1dc3374fe&consumer_secret=cs_496c7c0e476690d33725b29bc559b2dc1c701f66")
+        .then(resp => resp.json())
+        .then(resp => {
+            
+            this.setState({
+                produkts : resp
+            })
+        })
+    }
+}
+
